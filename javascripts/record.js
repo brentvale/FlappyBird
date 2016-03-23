8f4dd3aa7b9e56d987de0d47078439ecf0a3b70b
Brent Vale <brentvale@gmail.com>
2016-03-23 08:34:13 -0700

    adds favicon and styling for popup modal

--- javascripts/record.js
+++ javascripts/record.js
@@ -1,3 +1,6 @@
+var WIDTH_OF_MODAL = 250; //
+var HALF_WIDTH_OF_MODAL = WIDTH_OF_MODAL/2;
+
 function Record(height, width){
   this.height = height;
   this.width = width;
@@ -7,13 +10,36 @@ Record.prototype = {
   popUpModal: function(score){
     var div = document.createElement("div");
     div.className += "modal";
-    div.style.right = (this.width / 2) + "px";
+    div.style.right = ((this.width / 2) - HALF_WIDTH_OF_MODAL) + "px";
     div.style.top = (this.height / 2) + "px";
-    div.innerHTML = score;
+    div.innerHTML = "<br>" + "Your Score : " + score + "<br><br>";
     
     var input = document.createElement("input");
+    input.name = "name-field";
+    input.type = "text";
+    input.className += "modal-input";
 
+    var label = document.createElement("label");
+    label.for = "name-field";
+    label.innerHTML = "Enter Name" + "<br>";
+    
+    var optionsDiv = document.createElement("div");
+    var submitDiv = document.createElement("div");
+    var replayDiv = document.createElement("div");
+    
+    optionsDiv.className += "modal-options";
+    submitDiv.className += "modal-submit";
+    replayDiv.className += "modal-replay";
+    
+    submitDiv.innerHTML = "Submit";
+    replayDiv.innerHTML = "Replay";
+    
+    //add elements to DOM
     var divInserted = document.body.appendChild(div);
+    divInserted.appendChild(label);
     divInserted.appendChild(input);
+    var optionsInserted = divInserted.appendChild(optionsDiv);
+    optionsInserted.appendChild(submitDiv);
+    optionsInserted.appendChild(replayDiv);
   },
 };
